4d0f2a84269c0419ea3ef7c9643d9b450bbcd984
im-luka <lux.9833@gmail.com>
2023-11-05 19:13:47 +0100

    feat: added notification support

--- app/_components/providers/theme/index.tsx
+++ app/_components/providers/theme/index.tsx
@@ -1,6 +1,8 @@
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 import { FC, ReactNode } from "react";
 import { MantineProvider, createTheme } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 type Props = {
   children: ReactNode;
@@ -8,6 +10,14 @@ type Props = {
 
 export const ThemeProvider: FC<Props> = ({ children }) => {
   return (
-    <MantineProvider defaultColorScheme="light">{children}</MantineProvider>
+    <MantineProvider defaultColorScheme="light">
+      <Notifications
+        position="top-right"
+        limit={3}
+        containerWidth={250}
+        transitionDuration={500}
+      />
+      {children}
+    </MantineProvider>
   );
 };
